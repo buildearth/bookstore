02173ae7c057cce04e59816f5c8886f63272d53d
lixintao <lixintao2@huawei.com>
2021-04-28 17:34:19 +0800

    搜索功能完善

--- apps/manages/views.py
+++ apps/manages/views.py
@@ -1,6 +1,8 @@
 import random
 from django.shortcuts import render
 from django.views.generic.base import View
+from django.utils.decorators import method_decorator
+from django.db.models import Q
 from django.utils import timezone
 from apps.books import models as book_models
 from apps.manages import models as manage_models
@@ -8,6 +10,9 @@ from apps.manages import models as manage_models
 
 class IndexView(View):
     def get(self, request):
+        print(request.GET.get('search_key'))
+        search_key = request.GET.get('search_key')
+
         # 新书上架
         new_books = book_models.Book.get_new_books()
         # 热销推荐
@@ -20,6 +25,25 @@ class IndexView(View):
         carousels = manage_models.Carousel.get_carousels()
         # 一级类别
         categories = book_models.CategoryFirst.get_category_first()
+        if search_key:
+            search_books = book_models.Book.objects.filter(
+                Q(name__icontains=search_key)|
+                Q(author__icontains=search_key)|
+                Q(content_brief__icontains=search_key)
+            )
+
+            return render(
+                request, 'search_book.html', {
+                    'new_books': new_books,
+                    'recommend_hot_books': recommend_hot_books,
+                    'new_hot_books': new_hot_books,
+                    'hot_books': hot_books,
+                    'carousels': carousels,
+                    'categories': categories,
+                    'current_category': None,
+                    'search_books': search_books
+                })
+
         return render(
             request, 'index.html', {
                 'new_books': new_books,
